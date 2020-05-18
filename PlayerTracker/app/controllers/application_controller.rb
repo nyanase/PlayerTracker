419a86bdf4b3c880462fa205baa0674c5a92a81d
Nao <nao.yanase@wustl.edu>
2020-05-18 13:21:37 -0700

    added fields to forms, added permits

--- PlayerTracker/app/controllers/application_controller.rb
+++ PlayerTracker/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+protected
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :number, :coach) }
+  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :number, :coach) }
+end
 end
