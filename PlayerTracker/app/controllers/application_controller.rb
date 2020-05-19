eee938d6bbfffb7be1b8c99af24c8f8716365740
Nao <nao.yanase@wustl.edu>
2020-05-19 12:45:41 -0700

    removed number and coach field from users table

--- PlayerTracker/app/controllers/application_controller.rb
+++ PlayerTracker/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 protected
 
 def configure_permitted_parameters
-  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :number, :coach) }
-  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :number, :coach) }
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name) }
+  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name) }
 end
 end
