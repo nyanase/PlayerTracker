550b51e08886ea38140314598f11d14f8ca5cdd4
Nao <nao.yanase@wustl.edu>
2020-05-28 20:04:42 -0700

    added incomplete passes and cancel passes to tracker

--- PlayerTracker/app/javascript/packs/track.js
+++ PlayerTracker/app/javascript/packs/track.js
@@ -1,77 +1,85 @@
 import * as paper from 'paper';
 
 paper.install(window);
-// Keep global references to both tools, so the HTML
-// links below can access them.
-var tool1, tool2;
 
 window.onload = function() {
 	paper.setup('myCanvas');
 
-	// // Create two drawing tools.
-	// // tool1 will draw straight lines,
-	// // tool2 will draw clouds.
-
-	// // Both share the mouseDown event:
-	// var path;
-	// function onMouseDown(event) {
-	// 	path = new Path();
-	// 	path.strokeColor = 'black';
-	// 	path.add(event.point);
-	// }
-
-	// tool1 = new Tool();
-	// tool1.onMouseDown = onMouseDown;
-
-	// tool1.onMouseDrag = function(event) {
-	// 	path.add(event.point);
-	// }
-
-	// tool2 = new Tool();
-	// tool2.minDistance = 20;
-	// tool2.onMouseDown = onMouseDown;
-
-	// tool2.onMouseDrag = function(event) {
-	// 	// Use the arcTo command to draw cloudy lines
-	// 	path.arcTo(event.point);
-	// }
-
-	// Create a new path once, when the script is executed:
 	var tool = new Tool();
 
-	var path
+	var path;
 	var point;
+	var triangle;
+	var drawPoint;
 	var color = 'black';
 	var radius = 10;
-	var flag = false;
+	var pointFlag = false;
+	var incompleteFlag = false;
+
+	var trackerList = [];
+	var initPointList = [];
 
 	// on click draw line
 	tool.onMouseDown = function(event) {
-		if (!flag) {
+		if (!pointFlag) {
+			// remove previous tracker
+			if (path) {
+				drawPoint.remove();
+				path.remove();
+				triangle.remove();	
+			}
+			
+			// draw initial point of path
 			point = event.point;
-			flag = true;
+			drawPoint = new Path.Circle(point, 3);
+			drawPoint.fillColor = color;
+			initPointList.push({ x: point.x, y: point.y, complete: true });
+
+			pointFlag = true;
 		} else {
+			var completePass;
+
+			if (initPointList.slice(-1)[0].complete) {
+				completePass = true
+			} else {
+				completePass = false;
+			}
+
+			// draw path
 			path = new Path();
-			path.strokeColor = color;
+			if (completePass) {
+				path.strokeColor = color;
+			} else {
+				path.strokeColor = "red";
+			}
 			path.add(point);
 			path.add(event.point);
-			console.log("Point 1: " + point.x + ", " + point.y);
-			console.log("Point 2: " + event.point.x + ", " + event.point.y);
+			
+			// draw triangle
+			makeTriangle(point, event.point, completePass);
 
-			var angle = calcAngle(point, event.point);
-			var center = calcCenter(event.point, angle, radius);
-			console.log("Center: " + center.x + ", " + center.y);
+			
+			// store tracker info 
+			var tracker = { x1: point.x, y1: point.y, x2: event.point.x, y2: event.point.y, complete: completePass};
+			trackerList.push(tracker);
+			console.log("Tracker added!");
+			console.log(trackerList);
 
-			// triangle for arrow
-			var triangle = new Path.RegularPolygon(center, 3, radius);
-			triangle.fillColor = color;
-			var degAngle = radToDeg(angle);
-			console.log("Angle: " + degAngle);
-			rotateTriangle(triangle, degAngle, center);
-
-
-			flag = false;
+			pointFlag = false;
 		}
+	}
+
+	function makeTriangle(point1, point2, completePass) {
+		var angle = calcAngle(point1, point2);
+		var center = calcCenter(point2, angle, radius);
+		triangle = new Path.RegularPolygon(center, 3, radius);
+		if (completePass) {
+			triangle.fillColor = color;
+		} else {
+			triangle.fillColor = 'red';
+		}
+		var degAngle = radToDeg(angle);
+		rotateTriangle(triangle, degAngle, center);
 	}
 
 	function calcAngle(point1, point2) {
@@ -89,7 +97,6 @@ window.onload = function() {
 
 	function calcCenter(point, angle, radius) {
 		var r = radius / 2.0;
-		console.log("r: " + r);
 		var xCenter = point.x + r * Math.cos(angle);
 		var yCenter = point.y + r * Math.sin(angle);
 		var center = new Point(xCenter, yCenter);
@@ -108,5 +115,93 @@ window.onload = function() {
 			triangle.rotate(degAngle, center);
 		}
 	}
+
+	function markIncomplete() {
+		if (pointFlag) {
+			if (drawPoint) {
+				if (initPointList.slice(-1)[0].complete) {
+					drawPoint.fillColor = "red";
+					initPointList.slice(-1)[0].complete = false;
+				} else {
+					drawPoint.fillColor = color;
+					initPointList.slice(-1)[0].complete = true;
+				}
+			}
+		} else {
+			if (path) {
+				if (trackerList.slice(-1)[0].complete) {
+					trackerList.slice(-1)[0].complete = false;
+					initPointList.slice(-1)[0].complete = false;
+					path.strokeColor = 'red';
+					triangle.fillColor = 'red';
+					drawPoint.fillColor = 'red';
+				} else {
+					trackerList.slice(-1)[0].complete = true;
+					initPointList.slice(-1)[0].complete = true;
+					path.strokeColor = color;
+					triangle.fillColor = color;
+					drawPoint.fillColor = color;
+				}
+			}
+		}
+	}
+
+	function cancelPass() {
+		if (pointFlag) {
+			if (drawPoint) {
+				drawPoint.remove();
+				pointFlag = false;
+
+				if (trackerList.length > 0) {
+					// get both points for previous tracker
+					var point1 = new Point(trackerList.slice(-1)[0].x1, trackerList.slice(-1)[0].y1);
+					var point2 = new Point(trackerList.slice(-1)[0].x2, trackerList.slice(-1)[0].y2);
+
+					// draw previous point
+					drawPoint = new Path.Circle(point1, 3);
+
+					// draw previous path + triangle
+					path = new Path();
+					path.add(point1);
+					path.add(point2);
+					if (trackerList.slice(-1)[0].complete) {
+						path.strokeColor = color;
+						triangle.fillColor = color;
+						drawPoint.fillColor = color;
+					} else {
+						path.strokeColor = 'red';
+						drawPoint.fillColor = 'red';
+						triangle.fillColor = 'red';
+					}
+					makeTriangle(point1, point2);
+				}
+
+			}
+		} else {
+			if (path && trackerList.length > 0) {
+				path.remove();
+				triangle.remove();;
+				pointFlag = true
+
+				trackerList.pop();
+				console.log("hererere");
+			}
+		}
+	}
+
+	$('button#incomplete_button').on(
+		'click',
+		function() {
+			markIncomplete();
+		}
+	)
+
+	$('button#cancel_pass_button').on(
+    'click',
+    function() {
+      cancelPass();
+    }
+  );
+
 }
 
